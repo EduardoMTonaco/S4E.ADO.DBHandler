cec100c13c2a08311782b170f5a0f387a053c289
Eduardo Mariano Tonaco <111458446+EduardoMTonaco@users.noreply.github.com>
2023-01-27 11:29:06 -0300

    Adicionado criar tabelas e API eliminado alguns trechos de codigo da classe SQLserverconn que estavam inuteis Corrigido o nome da Solução e do Projeto

--- S4E.ADO/Data/SQLServerConn.vb
+++ S4E.ADO/Data/SQLServerConn.vb
@@ -13,11 +13,6 @@ Namespace Data
 
 
 #Region "CONSTRUTORES"
-        'Public Sub New(connectionString)
-        '    StringConn = connectionString
-        '    connDb = New SqlConnection(StringConn)
-        '    connDb.Open()
-        'End Sub
         Public Sub New()
             StringConn = "Data Source=DESKTOP-8KTP5PQ;Initial Catalog=S4E;Integrated Security=True"
             connDb = New SqlConnection(StringConn)
@@ -25,34 +20,6 @@ Namespace Data
         End Sub
 #End Region
 #Region "METODOS"
-
-        Public Function SQLQuery(SQL As String, ByRef DT As DataTable) As String
-            Try
-                Dim myCommand As IDbCommand = New SqlCommand(SQL, connDb)
-
-                Using myReader As IDataReader = myCommand.ExecuteReader
-                    Dim lista As IList(Of Object) = myReader
-                    DT.Load(myReader)
-                End Using
-
-                Return ""
-            Catch ex As Exception
-                Throw
-            End Try
-
-        End Function
-        Public Function SQLCommand(SQL As String) As String
-            Try
-                Dim myCommand As IDbCommand = New SqlCommand(SQL, connDb)
-
-                Dim myReader As IDataReader = myCommand.ExecuteReader
-                myReader.Close()
-                Return ""
-            Catch ex As Exception
-                Throw
-            End Try
-        End Function
-
         Protected Overridable Sub Dispose(disposing As Boolean)
             If Not disposedValue Then
                 If disposing Then
